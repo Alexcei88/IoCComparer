48572e2ad38ac70fecbab338e921677065afc7cf
shmakov.a <shmakov.a@skbkontur.ru>
2023-04-20 12:04:33 +0300

    Залил пустой проект для тестов

---
+++ tests/IoCComparer.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace IoCComparer.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
